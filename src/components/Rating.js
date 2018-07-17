ecbe351bf822576166542246ff36bed9e7e4d130
James Ferguson <ncjamesferguson@gmail.com>
2018-07-17 18:16:36 +0000

    automatically backed up by learn

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -11,12 +11,13 @@ class Rating extends React.Component {
     };
   }
 
-  componentWillReceieveProps = (nextProps) => {
+  componentWillReceiveProps(nextProps) {
     this.setState({
-      increasing: nextProps.rating > this.props.rating,
-      decreasing: nextProps.rating < this.props.rating
+      increasing: nextProps.rating > this.props.rating ? true : false,
+      decreasing: nextProps.rating < this.props.rating ? true : false
     });
   }
+
 
   render() {
     let trend = 'stable';
